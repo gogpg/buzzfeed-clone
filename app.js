a6c4fb48f7b2d7f39836ecb52eb43235b5375c9e
gogpg <giedre.luko@gmail.com>
2022-08-14 18:46:52 +0300

    result block and some styling

--- app.js
+++ app.js
@@ -25,10 +25,10 @@ const questions = [
                 credit: "Vikrant Mathure"
             },
             {
-                text: "Poland",
-                image: "https://images.unsplash.com/photo-1569166067304-9b5d8be29d1c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&q=crop&h=230&q=edges",
-                alt: "Riverside of Gdansk in Poland",
-                credit: "Ferdinand Stöhr"
+                text: "Lithuania",
+                image: "https://images.unsplash.com/photo-1549891472-991e6bc75d1e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&q=crop&h=230&q=edges",
+                alt: "Vilnius Old Town, Vilnius, Lithuania",
+                credit: "Igor Gubaidulin"
             }
         ]
     },
@@ -95,7 +95,42 @@ const questions = [
     }
 ]
 
-const populateQuestions = () => { 
+const unansweredQuestions = []
+const chosenAnswers = []
+const results = [
+    {
+        combination: ["Italy", "Soup", "Traditional"],
+        text: "Blue Cheese",
+        image: "https://images.unsplash.com/photo-1452195100486-9cc805987862?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=230&w=580&q=edges",
+        alt: "Blue Cheese",
+        
+    },
+    {
+        combination: ["Turkey", "Pancakes", "Treehouse"],
+        text: "Cheddar Cheese",
+        image: "https://images.unsplash.com/photo-1625084561186-4e619abad93b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=230&w=580&q=edges",
+        alt: "Cheddar cheese"
+       
+    },
+    {
+        combination: ["USA", "Toast", "Modern"],
+        text: "Brie Cheese",
+        image: "https://images.unsplash.com/photo-1634487359989-3e90c9432133?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=230&w=580&q=edges",
+        alt: "Brie cheese"
+        
+    },
+    {
+        combination: ["Lithuania", "Omelette", "Mansion"],
+        text: "Feta Cheese",
+        image: "https://images.unsplash.com/photo-1606735584785-1848fdcaea57?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=230&w=580&q=edges",
+        alt: "Feta cheese"
+        
+    }
+  
+]
+/// need to have default result to compensate for lack of combination data
+
+const populateQuestions = () => {
 
     questions.forEach(question => {   //prasuku cikla per questions masyva
 
@@ -113,41 +148,110 @@ const populateQuestions = () => {
         answersBlock.id = question.id + "-questions"       // atsakymo bloko id lygus atsakymu id, bet turi buti unikalus, todel dadedame kabutese zodii, kad nesikirst su auksciau priskirtu id
         answersBlock.classList.add('answer-options')
 
+        unansweredQuestions.push(question.id) //visi klausimai po pirmo ciklo eina i neatsakytus klausimus
+
         question.answers.forEach(answer => {    //dabar sukamas ciklas kiekvieno klausimo viduje
 
             const answerBlock = document.createElement('div') //sukuriamas divas kiekvienam atsakymui
             answerBlock.classList.add('answer-block')  //sukuriama klase, kad galetume parasyti stiliu
-            answerBlock.addEventListener('click', () => handleClick)   //sukuriame eventlisteneri, kad paspaudus ant atsakymo ivyktu nustaytas ivykis
+            answerBlock.addEventListener('click', () => handleClick(question.id, answer.text))   //sukuriame eventlisteneri, kad paspaudus ant atsakymo ivyktu nustaytas ivykis. parametrai klausimo id ir atsakymo tekstas
 
             const answerImage = document.createElement('img') //sukuriame elementa nuotraukai
             answerImage.setAttribute('src', answer.image)
             answerImage.setAttribute('alt', answer.alt)
-            
+
             const answerTitle = document.createElement('h3')  //sukuriame atsakymo pavadinima
             answerTitle.textContent = answer.text       //atsakymo antrastes tekstas lygus kiekvieno atsakymo per kuri sukamas ciklas tekstui
 
             const answerInfo = document.createElement('p') //sukuriame atsakymu informacijos bloka p.
 
-            const imgLink = document.createElement('a')  //kiekvienam atsakymui sukuriame nuorodos vieta
-            imgLink.setAttribute('href', answer.credit)  //jai prsiskiriame nuoroda ir jai priskiriame answer.credid, nuves i nuoroda, kas padare nuotrauka
+            const imageLink = document.createElement('a')  //kiekvienam atsakymui sukuriame nuorodos vieta
+            imageLink.setAttribute('href', answer.image)  //jai prsiskiriame nuoroda ir jai priskiriame answer.credid, nuves i nuoroda, kas padare nuotrauka
+            imageLink.textContent = answer.credit
 
             const sourceLink = document.createElement('a') //sukuriame dar vienos nuorodos vieta, kurioje bus nuoroda i saltini, is kurio paimta nuotrauka
             sourceLink.textContent = 'Unsplash' //jai priskiriamas tekstas atidaryti
             sourceLink.setAttribute('src', 'https://www.unsplash.com') //paspaudus ant unsplash atidarys saltinio nuoroda.
 
-            answerInfo.append(imgLink, ' to ', sourceLink) //i answer info sukrta elementa idedame nuotrauku nuorodas ir saltiniu nuorodas
+            answerInfo.append(imageLink, ' to ', sourceLink) //i answer info sukrta elementa idedame nuotrauku nuorodas ir saltiniu nuorodas
 
             answerBlock.append(answerImage, answerTitle, answerInfo) //i atsakymu bloka ikeliu sukurta atsakymu img ir pavadinima ir informacija.
-            
+
             answersBlock.append(answerBlock) //i atsakymu bloka dedame kiekvieno atsakymo bloka
         })
 
         questionDisplay.append(answersBlock)  //i klausimu lauka ikeliu atsakymu div
     })
-    
+
 }
 populateQuestions()
 
-const handleClick = () => {
-    console.log('clicked')
+const handleClick = (questionId, chosenAnswer) => {     //paspaudus ant pasirinkto paveikslelio console rodys klausimo id ir pasirinkto atsakymo teksta
+    if (unansweredQuestions.includes(questionId))   //paspaudus ant atsakymo, jeigu neatsakytu klausmu masyve yra klausimo id
+        chosenAnswers.push(chosenAnswer)    //tai i pasirintu atsakymu masyva ipushinti ta atsakyma, kuris susijes su klausimo id, ant kurio buvo paspauta
+    const itemToRemove = unansweredQuestions.indexOf(questionId)  //istrinamo neatsakytu klausimu indeksas yra klausimo id, is masyvo istriname to klausimo id, kuris jau yra atsakytas
+
+    if (itemToRemove > -1) {                        //jei trinamo klausimo indeksas yra daugiau uz -1
+        unansweredQuestions.splice(itemToRemove, 1) // is neatsakytu klausimu masyvo istriname ta klausima viena
+    }
+
+    console.log(chosenAnswers)
+    console.log(unansweredQuestions)
+
+    disableQuestionBlock(questionId, chosenAnswer) //////////////////////////////////////////////////////////////////////////////////////
+    //scroll to top most unanswered question
+    const lowestQuestionId = Math.min(...unansweredQuestions)  //surandame maziausia neatsakytu klausimu masyve esanti id
+    location.href = '#' + lowestQuestionId    //paspaudus ant atsakymo, mus is karto numeta ant kito klausimo, nes pries tai atsakytas klausimas jua yra isimtas is masyvo ir maziausias id yra sekantis klausimas.
+
+    if (!unansweredQuestions.length) {  //jeigu nera neatsakytu klausimu ilgio, jis jau yra tuscias, visi klausimai atsakyti
+        location.href = '#answer'  //kai atsakyti visi klausimai rodykle nusoka i atsakymo, rezultato langa
+        showResult()  
+    }
+}
+
+const showResult = () => {               //rezultatu funkcija
+    let result                           //rezultato kintamasis
+    results.forEach(answer => {          // ciklas per results masyva
+        if (chosenAnswers.includes(answer.combination[0]) +      //jei pasirinktu atsakymu masyve yra pirmas is combination elementas paskui antras ir tt
+            chosenAnswers.includes(answer.combination[1]) +
+            chosenAnswers.includes(answer.combination[2]) +
+            chosenAnswers.includes(answer.combination[3])) {
+
+                result = answer                                 //rezultatas vienas is results masyve esanciu atsakymu
+
+            } else if (!result) {   //jeigu nera tokio resultato
+                //paskutinis atsakymas bus defoltinis            //jei tokios kombinacijos nera, tada defoltinis variantas
+                result = results[3]
+            }
+    })
+
+    console.log(result)
+
+
+    const resultBlock = document.createElement('div')  //sukuriame resultato diva
+    resultBlock.classList.add('result-block')           //pridedame klase, kad galetume parasyti stiliu
+    const resultTitle = document.createElement('h3')     //sukuriame pavadinimo vieta
+    resultTitle.textContent = result.text                //pavadinimo tekstas- is results masyvo vieno is atsakymu tekstas
+    const resultImage = document.createElement('img')    //sukuriame vieta img
+    resultImage.setAttribute('src', result.image)        //img uzsetiname vieno is result nuotrauka
+    resultImage.setAttribute('alt', result.alt)
+
+    resultBlock.append('You are', resultTitle, resultImage)
+
+    answerDisplay.append(resultBlock)
+   
+
+   constAllAnswerBlocks = document.querySelectorAll('.answer-block')   ///padarome, kad po to, kai ivyksta resultatas nebegaletume spausti ant paveiksleliu ir rezultatas nesikartotu
+    Array.from(constAllAnswerBlocks).forEach(answerBlock => answerBlock.replaceWith(answerBlock.cloneNode(true)))
+
+}
+
+const disableQuestionBlock = (questionId, chosenAnswer) => {       //uzpilkiname nepasirinkta atsakyma
+    const currentQuestionBlock = document.getElementById(questionId + "-questions")   //paimami visi klausimai
+    
+    Array.from(currentQuestionBlock.children).forEach(block => {    //sukamas ciklas per ju vaikus, atsakymus
+        if (block.children.item(1).innerText != chosenAnswer) {     //imame atsakymu antra eilute, kur yra tekstas ir palyginame ar jis nesutampa su pasirinktu atsakymu
+            block.style.opacity = "50%"            //randame nepasirinkta klausima ir ji padarome blankiu
+        }
+    })
 }
